15275d43ed047f65108572b3331ab7bfbed69539
SirArslaan <arhamoja@gmail.com>
2014-08-14 17:36:51 +0200

    Added first test

---
+++ calculator_spec.js
@@ -0,0 +1,10 @@
+/*global describe, it */
+'use strict';
+
+(function () {
+  describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+      expect(true).toBe(true);
+    });
+  });
+})();
